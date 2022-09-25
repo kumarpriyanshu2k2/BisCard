eb97cc95df34e3e8df6cfef8b10ed562cc4bb417
kumarpriyanshu2k2 <kumar.priyanshu2k02@gmail.com>
2022-09-25 18:57:57 +0530

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -10,52 +10,95 @@ class Myapp extends StatelessWidget{
         home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Row(
-
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children:<Widget>[
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                  child: Text("Container 1"),
-                ),
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        height: 100,
-                        width: 100,
-                        color: Colors.yellow,
-
-                      ),
-                      Container(
-                        height: 100,
-                        width: 100,
-                        color: Colors.green,
-
-                      ),
-                    ],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/priyanshu.jpeg'),
+            ),
+                Text(
+                    'Priyanshu Kumar',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,),
+                  ),
+                Text('FLUTTER DEVELOPER',
+                  style:TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal.shade900,
                   ),
                 ),
-                Container(
-                  height: 100,
-                  width:100,
-                  child: Text("container 2"),
-                  color: Colors.blueAccent,
+                SizedBox(
+                  width:150.0,
+                  height:20.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
                 ),
+
+                Card(
+
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+ 44 123 456 789',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18.0,
+                      ),
+                    ),
+
+
+                  )
+                ),
+                Card(
+
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'Kumar.Priyanshu2k02@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 17.0,
+                        ),
+                      ),
+
+
+                    )
+                )
 
 
               ],
             ),
-
-
-            )
-
-
           ),
+
+           ),
+
+
+
+
     );
   }
 
